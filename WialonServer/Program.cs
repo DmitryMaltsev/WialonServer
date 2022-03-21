e488510da94dec277bc6c6db2e59755d4fedd487
DmitryMaltsev <kadukevich@mail.ru>
2022-03-21 16:58:24 +0300

    123

--- WialonServer/Program.cs
+++ WialonServer/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Services;
+
+using System;
+using System.IO;
 using System.Threading;
 
 using WialonServer.Models;
@@ -18,6 +21,9 @@ namespace WialonServer
             //threadListen.Start();
             WialonParsingService parsingService = new WialonParsingService();
             WialonDataModel wialonDataModel = parsingService.ParseData(parsingService.data);
+            string currentPath = Path.Combine(Directory.GetCurrentDirectory(), "Results","Data.json");
+            JsonService jsonService = new JsonService();
+            jsonService.WriteJS(currentPath, wialonDataModel);
         }
     }
 }
---
+++ WialonServer/Services/JsonService.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+using Microsoft.Win32;
+
+using Newtonsoft.Json;
+
+namespace Services
+{
+    public class JsonService
+    {
+
+
+        /// <summary>
+        /// Сериализует объект в json-файл
+        /// </summary>
+        /// <typeparam name="T">Тип объекта для сериализации</typeparam>
+        /// <param name="filePath">Куда сохранить</param>
+        /// <param name="settingsModel">Объект для сериализации</param>
+        public void WriteJS<T>(string filePath, T settingsModel)
+        {
+            if (string.IsNullOrEmpty(filePath)) return;
+           // if (!File.Exists(filePath)) File.Create(filePath);
+            string js = JsonConvert.SerializeObject(settingsModel, Formatting.Indented);
+            using (StreamWriter stream = File.AppendText(filePath))
+            {
+                stream.Write(js);
+            }
+        }
+
+        /// <summary>
+        /// Десериализует объект из json файла
+        /// </summary>
+        /// <typeparam name="T">Тип объекта для сериализации</typeparam>
+        /// <param name="filePath">Куда сохранить</param>
+        /// <param name="settingsModel">Объект для сериализации</param>
+        public T ReadJs<T>(string filePath, T trainData)
+        {
+            if (File.Exists(filePath))
+            {
+                using (StreamReader fs = new StreamReader(filePath))
+                {
+                    string fileString = fs.ReadToEnd();
+                    trainData = JsonConvert.DeserializeObject<T>(fileString);
+                }
+            }
+            return trainData;
+        }
+
+
+
+    }
+}
