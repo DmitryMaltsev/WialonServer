bd95802b1d48354b07be714fab0731e32401ad19
DmitryMaltsev <kadukevich@mail.ru>
2022-03-22 13:41:27 +0300

    123

--- WialonServer/Program.cs
+++ WialonServer/Program.cs
@@ -14,9 +14,6 @@ namespace WialonServer
 {
     class Program
     {
-        //private static IWialonParsingService _parsingService { get; set; }
-        //private static IJsonService _jsonService { get; set; }
-       // private static ITcpServerService _serverService { get; set; }
         static void Main(string[] args)
         {
             IWialonParsingService _parsingService = new WialonParsingService();
@@ -24,24 +21,6 @@ namespace WialonServer
             ITcpServerService _serverService = new TcpServerService(_jsonService, _parsingService); 
             Thread threadListen = new Thread(() => _serverService.StartLIstening(8888));
             threadListen.Start();
-
-            //System.Timers.Timer timer = new System.Timers.Timer(200);
-            //timer.Elapsed += OnTimedEvent;
-            //timer.AutoReset = true;
-         //   timer.Enabled = true;
-
         }
-
-        //private static void OnTimedEvent(Object source, ElapsedEventArgs e)
-        //{
-        //    foreach (ITcpClientservice client in _serverService.ClientsList)
-        //    {
-        //        if (client.IsDataRecieved )
-        //        {
-        //            WialonDataModel wialonDataModel = _parsingService.ParseData(client.ClientModel.RecievedDataList);
-        //            _jsonService.WriteJS(ReadWritePath.JsonPath, wialonDataModel);
-        //        }
-        //    }
-        //}
     }
 }
